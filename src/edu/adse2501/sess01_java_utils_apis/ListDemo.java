59dc0caa68abbdf702e630ea25dd7b20d22070b9
Kipp-irl <victorkoech017@gmail.com>
2026-01-20 12:01:17 +0300

    Initial Commit

---
+++ src/edu/adse2501/sess01_java_utils_apis/ListDemo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.adse2501.sess01_java_util_apis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Java class that demonstrates some of the concrete classes that implement the List
+ * interface from the java.util package and some of its methods.
+ * * @author Victor
+ */
+public class ListDemo {
+    
+    public static void main(String[] args) {
+        
+        List<String> fruits = new ArrayList<>();
+        
+        fruits.add("Apples");
+        fruits.add("Orange");
+        fruits.add("Mango");
+        fruits.add("Grapes");
+        fruits.add("Watermelon");
+        fruits.add("Passion Fruits");
+        fruits.add("Dragon Fruits");
+        fruits.add("Strawberries");
+        fruits.add("Bananas");
+        fruits.add("Paw paw");
+        fruits.add("Guavas");
+        fruits.add("Kiwi Fruits");
+        fruits.add("Pineapples");
+        fruits.add("Blueberries");
+        fruits.add("Grape Fruits");
+        fruits.add("Orange");
+        fruits.add("Apples");
+        fruits.add("Orange");
+       
+    }
+}
