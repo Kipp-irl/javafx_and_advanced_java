8941150660647267ead5af2cca5289937be7eb50
Kipp-irl <victorkoech017@gmail.com>
2026-01-21 11:36:42 +0300

    Initial Commit

---
+++ src/edu/adse2501/sess01_java_utils_apis/HashSetDemo.java
@@ -0,0 +1,102 @@
+package edu.adse2501.sess01_java_util_apis;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Java program to demonstrate the HashSet class and some of its methods.
+ * * @author Victor
+ */
+public class HashSetDemo {
+
+    public static void main(String[] args) {
+        // Initialize the set for design students
+        Set<String> designClass = new HashSet<>();
+
+        // Adding design students
+        designClass.add("James");
+        designClass.add("Kennedy");
+        designClass.add("Richard");
+        designClass.add("Michael");
+        designClass.add("Adam");
+        designClass.add("Ayub");
+        designClass.add("Osman");
+        designClass.add("Erick");
+        designClass.add("George");
+        designClass.add("Ema");
+        designClass.add("Salim");
+        designClass.add("Nadia");
+        designClass.add("Rachael");
+        designClass.add("Roocy");
+        designClass.add("Abigail");
+
+        // Create a string array of art students
+        String[] artStudents = {
+            "Roocy", "Julius", "Paul", "Amanda", "Kamau", "Leviathan", "Sean",
+            "Erick", "Joe", "Alejandro"
+        };
+
+        // Create a set from the art class string array
+        Set<String> artClass = new HashSet<>(Arrays.asList(artStudents));
+
+        // Create a set of all students in the campus
+        Set<String> allCampusStudents = new HashSet<>(designClass);
+        // Add the students from the art class as well
+        allCampusStudents.addAll(artClass);
+
+        // Display calls
+        displayDesignStudents(designClass);
+        displayArtStudents(artClass);
+        displayAllStudents(allCampusStudents);
+    }
+
+    /**
+     * Display the design class details using a for ... each loop.
+     * NB Works in java 7 & prior and current versions.
+     */
+    private static void displayDesignStudents(Set designClass) {
+        //
+        System.out.printf("""
+                          
+                          The design class has %d students and their names are:
+                          
+                          """, designClass.size());
+
+        for (Object obj : designClass) {
+            System.out.println(obj);
+        }
+    }
+
+    /**
+     * Display the art class details using a for ... each loop.
+     */
+    private static void displayArtStudents(Set artClass) {
+        // Following the same pattern as design class display
+        System.out.printf("""
+                          
+                          The art class has %d students and their names are:
+                          
+                          """, artClass.size());
+
+        for (Object obj : artClass) {
+            System.out.println(obj);
+        }
+    }
+
+    /**
+     * Display all students in the campus using a for ... each loop.
+     */
+    private static void displayAllStudents(Set allCampusStudents) {
+        // Following the same pattern for the total campus
+        System.out.printf("""
+                          
+                          The total campus has %d unique students and their names are:
+                          
+                          """, allCampusStudents.size());
+
+        for (Object obj : allCampusStudents) {
+            System.out.println(obj);
+        }
+    }
+}
--- src/edu/adse2501/sess01_java_utils_apis/ListDemo.java
+++ src/edu/adse2501/sess01_java_utils_apis/ListDemo.java
@@ -1,10 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.adse2501.sess01_java_util_apis;
 
+import edu.adse2501.classes.Student;
+import edu.adse2501.util.Spacer;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -13,11 +13,11 @@ import java.util.List;
  * * @author Victor
  */
 public class ListDemo {
-    
+
     public static void main(String[] args) {
-        
+
         List<String> fruits = new ArrayList<>();
-        
+
         fruits.add("Apples");
         fruits.add("Orange");
         fruits.add("Mango");
@@ -33,9 +33,60 @@ public class ListDemo {
         fruits.add("Pineapples");
         fruits.add("Blueberries");
         fruits.add("Grape Fruits");
-        fruits.add("Orange");
-        fruits.add("Apples");
-        fruits.add("Orange");
-       
+        fruits.add("Raspberries");
+        fruits.add("Pomegranate");
+        fruits.add("Avocadoes");
+        fruits.add("Tomatoes");
+        fruits.add("Sweet Melon");
+
+        // Display the above fruits using a for ... each loop
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        // Create a List of students from the classes package
+        List<Student> javaStudents = new ArrayList<>();
+
+        // Enroll/add students to the Java class
+        // Correct Order: regNo, course, names, nationality, birthDate, gender
+        javaStudents.add(new Student("EICN-0001", "Java Professional Programming", "Zamzam Ali", "Kenyan", LocalDate.of(2004, 11, 06), 'F'));
+        javaStudents.add(new Student("EICN-0002", "Java Professional Programming", "Fatima Adan", "Kenyan", LocalDate.of(2003, 05, 15), 'F'));
+        javaStudents.add(new Student("EICN-0003", "Java Professional Programming", "Victor Kiprotich", "Kenyan", LocalDate.of(2005, 9, 23), 'M'));
+        javaStudents.add(new Student("EICN-0005", "Java Professional Programming", "Adan Mbarak", "Kenyan", LocalDate.of(2004, 10, 10), 'F'));
+        javaStudents.add(new Student("EICN-0006", "Java Professional Programming", "Joel Kahure", "Kenyan", LocalDate.of(2003, 11, 07), 'F'));
+        javaStudents.add(new Student("EICN-0007", "Java Professional Programming", "Elizabeth Otieno", "Kenyan", LocalDate.of(1992, 8, 17), 'F'));
+        javaStudents.add(new Student("EICN-0008", "Java Professional Programming", "Peter Odhiambo", "Kenyan", LocalDate.of(2001, 7, 6), 'M'));
+        javaStudents.add(new Student("EICN-0009", "Java Professional Programming", "Stephen Muema", "Kenyan", LocalDate.of(1990, 11, 25), 'M'));
+        javaStudents.add(new Student("EICN-0010", "Java Professional Programming", "Reuben Kamau", "Kenyan", LocalDate.of(2003, 3, 15), 'M'));
+
+        // 2 students drop from the course
+        javaStudents.remove(1);
+        javaStudents.remove(6);
+
+        // Display the number of students left in the class
+        System.out.println("After the 2nd and 7th students dropped, the class "
+                + "has " + javaStudents.size() + " students.");
+
+        // Enroll 3 new students
+        javaStudents.add(new Student("EICN-0011", "C# Programming", "Sheila Josiah", "Uganda", LocalDate.of(1993, 8, 04), 'F'));
+        javaStudents.add(new Student("EICN-0012", "Python Machine Learning", "Noor Gupta", "India", LocalDate.of(2004, 06, 14), 'F'));
+        javaStudents.add(new Student("EICN-0013", "Java Professional Programming", "Tracy Chapman", "South Africa", LocalDate.of(2000, 10, 26), 'F'));
+
+        // Display the student details using an iterator
+        Spacer.separator();
+        System.out.println("The final list of students is");
+
+        Iterator<Student> stud = javaStudents.iterator();
+        int n = 0; //
+        while (stud.hasNext()) {
+            System.out.println("Displaying the details for student: " + (n + 1)); //
+            System.out.println(stud.next().toString()); //
+            n++; //
+            Spacer.separator();
+        }
+
+        // Display the final size of the students in the class
+        System.out.println("The final number of students after dropout and new "
+                + "enrollment is " + javaStudents.size());
     }
 }
